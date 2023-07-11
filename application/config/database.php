e4d9c8730316e4a2a3a61981761f0ab1a6c6098f
Bala0911 <134393829+Bala0911@users.noreply.github.com>
2023-07-11 16:47:58 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -64,10 +64,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
-	'database' => 'codeigniter-forum',
+	'hostname' => 'database-1.cl2hqh8vtfn7.us-east-2.rds.amazonaws.com',
+	'username' => 'admin',
+	'password' => 'Admin123',
+	'database' => 'testdb',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
